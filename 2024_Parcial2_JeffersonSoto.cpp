6a38f2e1d597cbc509bada34dcbf4ff1c3a958b4
TheVertrex <elvert3bras@gmail.com>
2024-05-04 10:00:47 -0600

    Ejercicio 1 finalizado Jefferson Soto

--- 2024_Parcial2_JeffersonSoto.cpp
+++ 2024_Parcial2_JeffersonSoto.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
-#include <windows.h>
+#include <fstream>
+#include <string>
 #include <filesystem>
 using namespace std;
+namespace fs = filesystem;
+
+void createFiles(int numFiles) {
+    string folderName = (numFiles % 2 == 0) ? "par" : "impar";
+    fs::path desktopPath = fs::path(getenv("USERPROFILE")) / "Desktop";
+    fs::create_directories(desktopPath / folderName);
+
+    for (int i = 1; i <= numFiles; ++i) {
+        string fileName = "file" + to_string(i) + ".txt";
+        ofstream file(desktopPath / folderName / fileName);
+        if (!file) {
+            cerr << "Error al crear el archivo: " << fileName << endl;
+        } else {
+            cout << "Archivo creado: " << fileName << endl;
+        }
+        file.close();
+    }
+}
+
 int main() {
     cout<<"Primer Ejercicio de Serie II"<<endl;
     cout<<"Crear una funcion que pregunte al usuario cuantos archivos desea crear, si la cantidad de archivos a crear es par, los archivos debe almacenarlos en el directorio de Escritorio y subcarpeta llamada par, de lo contrario si el numero proporcionado por el usuario es impar los archivos debe almacenarlos en el mismo directorio en unaa carpeta llamada impar, los archivos deben tener el nombre con el siguiente formato file1, file2, file3, etc."<<endl;
+    int numFiles;
+    cout << "Ingrese la cantidad de archivos a crear: ";
+    cin >> numFiles;
 
-
-
-
+    createFiles(numFiles);
 
     return 0;
 }
---
+++ SegundoEjercicioJeffersonSoto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+using namespace std;
+namespace fs = filesystem;
+
+
+int main() {
+    cout<<"Segundo Ejercicio de Serie II"<<endl;
+    cout<<"Crear una funcion que agregue texto a los archivos creados, los archivos deben tener la misma cantidad de lineas segun su numero de archivo por ejemplo: file1 debe tener una linea de texto en su contenido, file2 debe tener 2 lineas y asi sucesivamente."<<endl;
+    cout<<""<<endl;
+
+
+    return 0;
+}
