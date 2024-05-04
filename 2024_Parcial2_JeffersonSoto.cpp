4543572d9f79de7cf211b090e9266dcf8fa4fb27
TheVertrex <elvert3bras@gmail.com>
2024-05-04 09:41:34 -0600

    2024_Parcial2_JeffersonSoto

---
+++ 2024_Parcial2_JeffersonSoto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <filesystem>
+using namespace std;
+int main() {
+    cout<<"Primer Ejercicio de Serie II"<<endl;
+    cout<<"Crear una funcion que pregunte al usuario cuantos archivos desea crear, si la cantidad de archivos a crear es par, los archivos debe almacenarlos en el directorio de Escritorio y subcarpeta llamada par, de lo contrario si el numero proporcionado por el usuario es impar los archivos debe almacenarlos en el mismo directorio en unaa carpeta llamada impar, los archivos deben tener el nombre con el siguiente formato file1, file2, file3, etc."<<endl;
+
+
+
+
+
+    return 0;
+}
